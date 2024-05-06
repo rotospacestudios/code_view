f167e3641b551d64da8ac474b6060c3ee695767b
rotospacestudios <rotospacestudios@gmail.com>
2024-05-06 14:30:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk, filedialog, messagebox, simpledialog
 from qt_delegates import ListExplorer
 from file_type_explorer import FileTypeExplorer
 from core_editor import CoreEditor
-from file_parser import ParserManager, LuaFileParser, FileParser, CParser,CPlusPlusParser, PythonParser,FileParser
+from file_parser import ParserManager, LuaFileParser, FileParser, CParser,CPlusPlusParser, PythonFileParser,FileParser
 import os
 from file_types_module import file_types
 import atexit
